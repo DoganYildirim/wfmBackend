de297f893594ed86a01c4ea1531a1e72c695cf6a
DoganYildirim <24220738+DoganYildirim@users.noreply.github.com>
2024-12-25 16:11:55 +0100

    Add files via upload

---
+++ wfmbackend/src/main/java/wfm/solution/wfmbackend/WfmbackendApplication.java
@@ -0,0 +1,13 @@
+package wfm.solution.wfmbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WfmbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WfmbackendApplication.class, args);
+	}
+
+}
---
+++ wfmbackend/src/test/java/wfm/solution/wfmbackend/WfmbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package wfm.solution.wfmbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WfmbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
